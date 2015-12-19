26794b1921fc5b13c7493bab0b1c47afc9ebf2bb
E Rendon <x_ernie_x@hotmail.com>
2015-12-18 19:55:58 -0700

    init

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,18 +1,21 @@
-$(function(){
-    
-    $(window).smartload(function(){
+$(function() {
+
+    $(window).smartload(function() {
         "use strict";
-        
+
         // Set your google maps parameters
-        var $latitude = -6.193948, // If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-            $longitude = 106.849184,
-            $map_zoom = 16; // Zoom setting
+        // // If you unable to find latitude and longitude of your address. 
+        // Please visit http://www.latlong.net/convert-address-to-lat-long.html 
+        // you can easily generate latitue and longitude.
+        var $latitude = 33.625761,
+            $longitude = -112.019004,
+            $map_zoom = 10; // Zoom setting
 
         // Google map custom marker icon
         var $marker_url = 'assets/img/map-marker-icon.png';
-        
+
         // We define here the style of the map (https://snazzymaps.com/style/38/shades-of-grey)
-        var style = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+        var style = [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}];
 
         // Set google map options
         var map_options = {
@@ -26,11 +29,11 @@ $(function(){
             scrollwheel: false,
             styles: style
         };
-        
+
         // Inizialize the map
         var map = new google.maps.Map(document.getElementById('google-container'), map_options);
-        
-        // Add a custom marker to the map				
+
+        // Add a custom marker to the map               
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng($latitude, $longitude),
             map: map,
