4d82827a1502961d9755afe0ca283833878c838c
E Rendon <x_ernie_x@hotmail.com>
2015-12-20 20:46:58 -0700

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,46 +77,9 @@ $(function(){
         stickyMenu();
     });
     
+
     
-    /*-------------------------------------------------------------------*/
-    /*  6. Navbar dropdown opening on hover,
-    /*  and opening on click for collapsed navbar.
-    /*-------------------------------------------------------------------*/
-    var toggleNavbarMethod = function(){
-        var ww = Math.max($(window).width(), window.innerWidth),
-        dropdown = $('.navbar .dropdown');
-        
-        if (ww >= 992){
-            dropdown.on('mouseover', function(){
-                if (!$(this).hasClass('open')){
-                    $(this).addClass('open');
-                }
-            }).on('mouseout', function(){
-                if ($(this).hasClass('open')){
-                    $(this).removeClass('open');
-                }
-            });
-        }
-        else {
-            dropdown.off('mouseover').off('mouseout');
-        }
-    };
-    
-    toggleNavbarMethod();
-    
-    // Call toggleNavbarMethod(); when window is resized.
-    $(window).smartresize(function(){
-        toggleNavbarMethod();
-    });
-    
-    
-    /*-------------------------------------------------------------------*/
-    /*  7. Prevent bootstrap dropdown closing when clicked.
-    /*-------------------------------------------------------------------*/
-    $('.dropdown-menu').click(function(e){
-        e.stopPropagation();
-    });
-    
+ 
  
     /*-------------------------------------------------------------------*/
     /*  8. Portfolio gallery. Requires jQuery Magnific Popup plugin.
