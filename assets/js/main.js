55e0b4825dc1a0c343d87ae6dbb27a6821bbe62a
E Rendon <x_ernie_x@hotmail.com>
2015-12-28 13:09:49 -0700

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,35 +14,6 @@ $(function(){
 
     
     
-    // /*-------------------------------------------------------------------*/
-    // /*  4. Page scrolling feature, requires jQuery Easing plugin.
-    // /*-------------------------------------------------------------------*/
-    // var pageScroll = function(){
-    //     $('.page-scroll > a').bind('click', function(e){
-    //         e.preventDefault();
-            
-    //         var anchor = $(this),
-    //         href = anchor.attr('href'),
-    //         offset = $('body').attr('data-offset');
-            
-    //         $('html, body').stop().animate({
-    //             scrollTop: $(href).offset().top - (offset - 1)
-    //         }, 1500, 'easeInOutExpo');
-            
-    //         /*
-    //          * Automatically retract the navigation after clicking 
-    //          * on one of the menu items.
-    //          */
-    //         if(!$(this).parent().hasClass('dropdown')){
-    //             $('.berg-collapse').collapse('hide');
-    //         }
-    //     });
-    // };
-    
-    
-    // pageScroll();
-    
-    
     /*-------------------------------------------------------------------*/
     /*  5. Make navigation menu on your page always stay visible.
     /*  Requires jQuery-Sticky plugin.
