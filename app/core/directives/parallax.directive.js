a3c6750b7f9a0ead1e28a66f3f604583fd983372
E Rendon <x_ernie_x@hotmail.com>
2015-12-29 12:30:42 -0700

    init

--- app/core/directives/parallax.directive.js
+++ app/core/directives/parallax.directive.js
@@ -19,14 +19,20 @@
 		return directive;
 
 		function link(scope, element, attrs, ngModel) {
+			if (!attrs.id) {
+				throw new Error("Missing a required attribute 'id'.");
+			}
+
 			if (jQuery.browser.mobile === false) {
 				// This is for parallax using Skrollr
 				var s = skrollr.init({
 					forceHeight: false
 				});
 
+				var  jQueryElementIDSelector = "#"+attrs.id+" > .parallax";
+
 				// Refresh skrollr after resizing our sections
-				s.refresh($('.parallax'));
+				s.refresh($(jQueryElementIDSelector));
 
 				//skrollr.init().refresh();
 			}
