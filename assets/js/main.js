f93311ac4a44571da0d585ef44f9b115221d70ec
E Rendon <x_ernie_x@hotmail.com>
2016-01-03 22:31:47 -0700

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,15 +11,31 @@ $(function() {
         //NOTE because jpreloader plugin affects the PhantomJS testing. We switched to a 
         //basic if check in main.js And in the test runner this plugin is diabled on purpose. 
         if (jpreLoaderObj) {
-            $('body').jpreLoader({
+            var options = {
                 showPercentage: false,
                 loaderVPos: '50%',
                 autoClose: true,
                 debugMode: true
-            });
-        } 
+            };
+            $('body').jpreLoader(options, imageTextCallback);
+        } else {
+            //the jquery jpreloader plugin was not found
+            //just wait N seconds and then show img cell content
+            setTimeout(imageTextCallback, 2000);
+        }
     });
 
 
 
+    /**
+     * This method is to show bg-img-table-cell content once the browser has loaded images.
+     * But if the jquery jpreloader plugin was not found we should still calle this method.
+     */
+    var imageTextCallback = function() {
+        $(".bg-img-table-cell").each(function(index, element) {
+            $(element).css("display", "table-cell");
+            $(element).addClass( "animated pulse" );
+        });
+        return true;
+    };
 });
