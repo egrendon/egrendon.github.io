4d7b3d95217276167c332adb6242b8b7856a3acc
E Rendon <x_ernie_x@hotmail.com>
2016-01-27 20:42:14 -0700

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,26 +4,26 @@ $(function() {
     /*  1. Preloader. Requires jQuery jpreloader plugin.
     /*  http://www.inwebson.com/jquery/jpreloader-a-preloading-screen-to-preload-images/
     /*-------------------------------------------------------------------*/
-    $(document).ready(function() {
+    // $(document).ready(function() {
 
-        var jpreLoaderObj = $.fn.jpreLoader;
+    //     var jpreLoaderObj = $.fn.jpreLoader;
 
-        //NOTE because jpreloader plugin affects the PhantomJS testing. We switched to a 
-        //basic if check in main.js And in the test runner this plugin is diabled on purpose. 
-        if (jpreLoaderObj) {
-            var options = {
-                showPercentage: false,
-                loaderVPos: '50%',
-                autoClose: true,
-                debugMode: true
-            };
-            $('body').jpreLoader(options, imageTextCallback);
-        } else {
-            //the jquery jpreloader plugin was not found
-            //just wait N seconds and then show img cell content
-            setTimeout(imageTextCallback, 2000);
-        }
-    });
+    //     //NOTE because jpreloader plugin affects the PhantomJS testing. We switched to a 
+    //     //basic if check in main.js And in the test runner this plugin is diabled on purpose. 
+    //     if (jpreLoaderObj) {
+    //         var options = {
+    //             showPercentage: false,
+    //             loaderVPos: '50%',
+    //             autoClose: true,
+    //             debugMode: true
+    //         };
+    //         $('body').jpreLoader(options, imageTextCallback);
+    //     } else {
+    //         //the jquery jpreloader plugin was not found
+    //         //just wait N seconds and then show img cell content
+    //         setTimeout(imageTextCallback, 2000);
+    //     }
+    // });
 
 
 
